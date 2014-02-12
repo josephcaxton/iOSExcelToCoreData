5c8e25b7cfc2c26272177b9a722725a425953094
josephcaxton <Josephcaxtonidowu@yahoo.com>
2014-02-12 10:32:31 +0000

    First Commit
    iOSCoreData

---
+++ iOSExcelToCoreData/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSExcelToCoreData
+//
+//  Created by Editor on 12/02/2014.
+//  Copyright (c) 2014 JLCI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
